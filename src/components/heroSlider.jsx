62399801ee410ad898726f5f1e733a9a7f35ac51
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2025-12-16 22:33:07 +0200

    .

--- src/components/heroSlider.jsx
+++ src/components/heroSlider.jsx
@@ -26,18 +26,18 @@ const HeroSlider = () => {
           </SwiperSlide>
           <SwiperSlide>
             <div className="content">
-              <h4>Introducing the new</h4>
-              <h3>Microsoft Xbox <br /> 360 Controller</h3>
-              <p>Windows 11/10/8/7/Xp, PS3, Tv Box</p>
+              <h4>M/N1-X6U SPEAKER</h4>
+              <h3>Led Bluetooth <br />Speaker Lamp</h3>
+              <p>support 3.5 mm jack audio input</p>
               <Link to='/' className='btn'>Shop Now</Link>
             </div>
             <img src={banner2} alt="banner2" />
           </SwiperSlide>
           <SwiperSlide>
             <div className="content">
-              <h4>Introducing the new</h4>
-              <h3>Microsoft Xbox <br /> 360 Controller</h3>
-              <p>Windows 11/10/8/7/Xp, PS3, Tv Box</p>
+              <h4>NEW ARRIVAL</h4>
+              <h3>Xiaomi Air 75 <br /> Earbuds</h3>
+              <p>AAC HD Sound Quality</p>
               <Link to='/' className='btn'>Shop Now</Link>
             </div>
             <img src={banner3} alt="banner3" />
