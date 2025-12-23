a1e4229dd05c30f5a47d97ba13b5ecbcc7995b65
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2025-12-23 12:15:05 +0200

    css edits in toast-wrapper to make it responsive and make product in cart open product page

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { FaTrashAlt } from "react-icons/fa";
 import { CartContext } from '../context/cartContext';
+import { Link } from 'react-router-dom';
 
 
 const CartItem = ({item}) => {
@@ -10,12 +11,16 @@ const CartItem = ({item}) => {
   return ( 
     <div className="cart-item">
       <div className="item-info">
-        <div className="image-item">
-          <img src={item.images[0]} alt="" />
-        </div>
+        <Link to={`/product/${item.id}`}>       
+          <div className="image-item">
+            <img src={item.images[0]} alt="" />
+          </div>
+        </Link>
         <div className="item-content">
-          <h4>{item.title}</h4>
-          <p className="price">${item.price}</p>
+          <Link to={`/product/${item.id}`}>          
+            <h4>{item.title}</h4>
+            <p className="price">${item.price}</p>
+          </Link>
           <div className="quantity-control">
             <button onClick={()=> item.quantity <= 1 ? '' : decreaseQuantity(item) }>-</button>
             <span className='quantity'>{item.quantity}</span>
