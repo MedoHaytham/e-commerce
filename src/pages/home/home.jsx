7e806edb955dfcaa1e6a875e40d59477ad0cd3c3
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2025-12-24 14:14:22 +0200

    edits on home page on loading

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -16,35 +16,9 @@ const homeCate = [
   "sports-accessories",
 ];
 
-const breakpoints = {
-  340: 3,
-  370: 3,
-  520: 4,
-  767: 5,
-  992: 5,
-};
-
-
 const HomePage = () => {
-
   const [homeCategories, setHomeCategories] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [slidesCount, setSlidesCount] = useState(3);
-
-  const updateSlidesCount = () => {
-    const width = window.innerWidth;
-    let count = 3;
-    for (let bp in breakpoints) {
-      if (width >= bp) count = breakpoints[bp];
-    }
-    setSlidesCount(count);
-  };
-
-  useEffect(() => {
-    updateSlidesCount();
-    window.addEventListener('resize', updateSlidesCount);
-    return () => window.removeEventListener('resize', updateSlidesCount);
-  }, []);
 
   useEffect(() =>{
     async function fetchCategories () {
@@ -74,14 +48,13 @@ const HomePage = () => {
     <>
       <HeroSlider />
       {
-        loading ? (
-          homeCate.map((_, idx) => (
-            <SlideProductsLoading key={idx} count={slidesCount} />  
+        loading 
+        ? homeCate.map((_,index) => (
+            <SlideProductsLoading key={index} count={3} />  
           ))
-        ) :
-        homeCategories.map((hc) => (
-          <SlideProducts key={hc.slug} categorySlug={hc.slug} categoryName={hc.name} />
-        ))
+        : homeCategories.map((hc) => (
+            <SlideProducts key={hc.slug} categorySlug={hc.slug} categoryName={hc.name} />
+          ))
       }
     </>
   );
