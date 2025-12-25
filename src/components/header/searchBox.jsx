56e752418ca2de155b09a8a77620dfc03005e78d
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2025-12-25 12:21:32 +0200

    css edits

--- src/components/header/searchBox.jsx
+++ src/components/header/searchBox.jsx
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const SearchBox = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(()=> {
+    async function fetchSuggestions() {
+      try {
+        let response = await axios.get(`https://dummyjson.com/products/search?q=${searchTerm}`);
+        let data = response.data.products.map((p) => ({
+          id: p.id,
+          title: p.title,
+          price: p.price,
+          rating: p.rating,
+          images: p.images,
+        }));
+        setSuggestions(data.slice(0, 6));
+      } catch (error) {
+        toast.error('Error on fetch suggestions: ' + error)
+      }
+    }
+  }, [searchTerm])
+
+  console.log(suggestions);
 
   let submitHadnler = (e) => {
     e.preventDefault();
