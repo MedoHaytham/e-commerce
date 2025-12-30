7304978a1d05d4d0a335ae3cd51c28b140b8b7ff
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2025-12-30 15:18:33 +0200

    add accessoriesPage

--- src/pages/accessories/accessoriesPage.jsx
+++ src/pages/accessories/accessoriesPage.jsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageTransition from '../../components/pageTransition';
+import AccessoriesPageLoading from './accessoriesPageLoading';
+import Product from '../../components/slideProducts/product';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import TopSlide from '../../components/topSlide';
+
+import './accessoriesPage.css';
+
 
 const AccessoriesPage = () => {
-  return ( 
-    <PageTransition>
-      <div className='accessories'>
-        <h1>Accessories Page</h1>
-      </div>
-    </PageTransition>
+
+  const [accessories, setAccessories] = useState([]);
+  const [accessoriesPageLoading, setAccessoriesPageLoading] = useState(true);
+  
+
+  useEffect(() => {
+    async function fetchAccessories() {
+      try {
+        let response = await axios.get('https://dummyjson.com/products/categories');
+        let categories = response.data.map((c) => c.slug);
+        let accessoriesCat = categories.filter((c) => c.toLowerCase().includes('accessories'));
+
+        const allAccessories = [];
+
+        for (let category of accessoriesCat) {
+          let response = await axios.get(`https://dummyjson.com/products/category/${category}`);
+          allAccessories.push({
+            category: category,
+            products: response.data.products
+          });
+        }
+        setAccessories(allAccessories);
+      }catch(error) {
+        toast.error('Error on fetchAccessories' + error);
+      }finally {
+        setAccessoriesPageLoading(false);
+      }
+    } 
+    fetchAccessories();
+  },[]);
+
+  return (
+    accessoriesPageLoading 
+    ? <AccessoriesPageLoading count={3}/>
+    : <PageTransition>
+        <div className='accessories'>
+          <div className="container">
+            {
+              accessories.map((a, index) => (
+                <div className='accessory' key={index}>
+                  <TopSlide categoryName={a.category}/>
+                  <div className="products">
+                    {
+                      a.products.map((p) => (<Product key={p.id} item={p} />))
+                    }
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </PageTransition>
   );
 }
 
---
+++ src/pages/accessories/accessoriesPageLoading.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from 'react';
+
+const AccessoriesPageLoading = ({count}) => {
+  return ( 
+    <div className='accessories-loading'>
+      <div className="container">
+        <div className="accessory">
+          <div className='top-slider'>
+            <h2 className='skeltoin'></h2>
+            <p className='skeltoin'></p>
+          </div>
+          <div className="products-loading">
+            { 
+              Array.from({length: count}).map((_,index) => (
+                <div key={index} className='product'>
+                  <div className="image skeltoin"></div>
+                  <div className="content skeltoin"></div>
+                  <div className="content skeltoin"></div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AccessoriesPageLoading;
