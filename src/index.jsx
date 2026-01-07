f1685b4c60d3a4c2543a7a9a258fd1560456588c
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-01-07 11:41:00 +0200

    update index

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
-import CartProvider from './context/cartContext';
-import FavoritesProvider from './context/favoritesContext';
+import CartProvider from './context/cartContext.jsx';
+import FavoritesProvider from './context/favoritesContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
