ef7db28c271df226bbcab6ae078d0efd056f9ac6
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-01-10 15:47:08 +0200

    prevent going to Checkout Page if cartItems.length equal 0

--- src/pages/checkout/checkoutPage.jsx
+++ src/pages/checkout/checkoutPage.jsx
@@ -2,9 +2,25 @@ import './checkoutPage.css';
 import PageTransition from '../../components/pageTransition';
 import OrderSummary from './orderSummary';
 import ShipingMethod from './shipingMethod';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 
 const CheckoutPage = () => {
+
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    if(cartItems.length === 0) {
+      navigate('/cart');
+    }
+  },[cartItems,navigate]);
+
+  if(cartItems.length === 0) {
+    return null;
+  }
 
   return ( 
     <PageTransition>
